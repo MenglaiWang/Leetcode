125c3e557754240085b2fcce1aa9357bb356fa46
WangMenglai <wangmenglai91@gmail.com>
2016-07-26 16:28:26 +0800

    Add files via upload

--- 331. Verify Preorder Serialization of a Binary Tree.cpp
+++ 331. Verify Preorder Serialization of a Binary Tree.cpp
@@ -24,3 +24,30 @@ public:
     		ret.push_back(s.substr(last));
     }
 };
+
+
+//方法二：stack
+class Solution {
+public:
+    bool isValidSerialization(string preorder) {
+        stack<char> stk;
+        preorder.push_back(',');
+        bool is_num = false;
+        for(auto c : preorder){
+            if(c == '#'){
+                while(stk.size() && stk.top() == '#'){
+                    stk.pop();
+                    if(stk.empty() || stk.top() == '#') return false;
+                    stk.pop();
+                }
+                stk.push('#');
+            } else if( c == ','){
+                if(is_num) stk.push('n');
+                is_num = false;
+            } else{
+                is_num = true;
+            }
+        }
+        return stk.size()==1 && stk.top() == '#';
+    }
+};
